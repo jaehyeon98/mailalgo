7951dfaa19b387a377646f3c20a7067dc50737ef
고재현 <joali9807@naver.com>
2022-03-17 23:21:46 +0900

    0317 고재현

--- 고재현/week1_brute_force/18511_큰 수 구성하기.py
+++ 고재현/week1_brute_force/18511_큰 수 구성하기.py
@@ -1,4 +1,35 @@
-n, k = map(int,input().split())
-lst = list(map(int,input().split()))
+# pypy보다 python이 더 빠르네?
+def perm(a,b):
+    global k
+    global max_val
+    if a == b:
+        ans1 = int(''.join(ans))
+        if ans1 > n:
+            return
+        if ans1 > max_val:
+            max_val = ans1
+        elif ans1 < max_val:
+            return
 
-perm()
+    else:
+        for i in range(k-1,-1,-1):
+            ans.append(lst[i])
+            perm(a,b+1)
+            ans.pop()
+
+
+n, k = map(int,input().split())
+lst = list(input().split())
+l = len(str(n))
+ans = []
+max_val = 0
+for i in range(l,0,-1):
+    perm(i,0)
+print(max_val)
+
+
+
+
+        
+
+
---
+++ 고재현/week1_brute_force/A와 B 2.py
@@ -0,0 +1,9 @@
+def check(a,n):
+    if s == t:
+        pass
+    elif a > n:
+        
+
+s = input()
+t = input()
+n = len(t)
