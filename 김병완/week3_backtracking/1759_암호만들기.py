b77373a0edce1c8a81b3552e73ca3acb1dd3ec8d
Byeongwan <quddhs4694@naver.com>
2022-03-30 23:27:15 +0900

    0330 김병완

--- 김병완/week3_backtracking/1759_암호만들기.py
+++ 김병완/week3_backtracking/1759_암호만들기.py
@@ -1,40 +1,47 @@
 import sys
 sys.setrecursionlimit(10 ** 8)
+from itertools import combinations
 input = sys.stdin.readline
 
 L, C = map(int, input().split())
 
 codes = input().split()
+codes.sort()
 vowels = {'a', 'e', 'i', 'o', 'u'}
-visit = [False] * C
-answer = set()
+answer = []
 
-def dfs(s, cur_str, v, c):
-    if L - s <= 1:
-        if v < 1: return
-    elif L - s <= 2:
-        if c < 2: return
-    if s == L:
-        answer.add(''.join(sorted(cur_str)))
-        return
-    else:
-        for i in range(C):
-            if visit[i]: continue
-            if codes[i] in vowels:
-                nv = v + 1
-                nc = c
-            else:
-                nv = v
-                nc = c + 1
+# def dfs(s, cur_str, v, c):
+#     if s >= C : return
+#     if len(cur_str) == L:
+#         if v >= 1 and c >= 2:
+#             answer.append(''.join(sorted(cur_str)))
+#         return
+#     else:
+#         nv = v
+#         nc = c
+#         if codes[s] in vowels:
+#             nv += 1
+#         else:
+#             nc += 1
+#         nex_str = cur_str[:]
+#         nex_str.append(codes[s])
+#         dfs(s + 1, nex_str, nv, nc)
+#         dfs(s + 1, cur_str, v, c)
 
-            visit[i] = True
-            nex_str = cur_str[:]
-            nex_str.append(codes[i])
-            dfs(s + 1, nex_str, nv, nc)
-            visit[i] = False
+candidates = list(combinations(codes, L))
+# print(candidates)
+for candidate in candidates:
+    vc = [0] * 2
+    for string in candidate:
+        if string in vowels:
+            vc[0] += 1
+        else:
+            vc[1] += 1
+    if vc[0] >= 1 and vc[1] >= 2:
+        answer.append(''.join(candidate))
 
 
-dfs(0, [], 0, 0)
-result = sorted(answer)
-print('\n'.join(result))
+# dfs(0, [], 0, 0)
+# result = sorted(answer)
+print('\n'.join(answer))
 
